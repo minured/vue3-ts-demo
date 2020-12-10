dd07ad75f01197f4c3fba7946475702e1bbbbee7
minured <minured@qq.com>
2020-12-10 16:39:54 +0800

    money页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="nav-wrapper">
+  <div class="nav">
     <router-link to="/label">label</router-link> |
     <router-link to="/money">money</router-link> |
     <router-link to="/statistics">statistics</router-link>
@@ -12,9 +12,10 @@ import { Options, Vue } from "vue-class-component";
 })
 export default class Nav extends Vue {}
 </script>
-<style lang="scss">
-.nav-wrapper {
-
+<style lang="scss" scoped>
+.nav {
     border-top: 1px solid rgba(0,0,0,0.1);
+    display: flex;
+    justify-content: space-around;
 }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,50 @@
 <template>
-  <div>
-    Money
-  </div>
-  <div>
-    <Nav />
+  <div class="money">
+    <div class="money-content">
+      Money
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>last</div>
+    </div>
+    <div class="nav-wrapper">
+      <Nav />
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -11,9 +52,23 @@ import { Options, Vue } from "vue-class-component";
 
 @Options({
   props: {},
-  components: {
-  },
+  components: {},
 })
 export default class Money extends Vue {}
 </script>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.money {
+  height: 100vh;
+  background: gray;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  .money-content {
+      flex: 1;
+      overflow: auto;
+  }
+  .nav-wrapper {
+
+  }
+}
+</style>
